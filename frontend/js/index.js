e535d3e2015714193b21c56ea16945a7ea6f8b3b
Vini Guimarães <niciusfg@gmail.com>
2022-07-07 12:13:54 -0700

    js changes

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -72,6 +72,14 @@ $(() => {
       });
 
 
+      $('#newTransaction').on('submit', (e) => {
+        e.preventDefault()
+        console.log("clicked")
+
+        if($('#transactionAmount').val() <= 0) return false
+        if(!$("[name=radioValue]:checked")) return false
+
+      });
 
 
     });  
